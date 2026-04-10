1f9d861b9af497083c8ed5065b684b6e17b5f1ed
Utkarsh Sparx <92734266+iamutkarsh25@users.noreply.github.com>
2026-04-10 03:54:34 +0000

    Validate

--- lambda/test_lambda/app.py
+++ lambda/test_lambda/app.py
@@ -1,3 +1,6 @@
 def handler(event, context):
-   print("Hi! This is the first code to deploy.")
-    
+    print("Hi! This is the first code to deploy.")
+    return {
+        "statusCode": 200,
+        "body": "OK"
+    }
